58b7c040135f3b6647b6f693706c06920a3e774e
GitHolo <ooye2332@gmail.com>
2024-10-08 20:31:37 +0200

    fix 4.2

--- 3.2.py
+++ 3.2.py
@@ -1,8 +1,8 @@
-with open("liczby.txt", "r") as file:
+with open("liczby_przyklad.txt", "r") as file:
     li = []
     lines = file.readlines()
     first_row = lines[0]
     for line in first_row.split():
-        li.append(int(line.strip()))
+        li.append(int(line))
     li.sort(reverse=True)
-    print(li[110])
+    print(li[100])
